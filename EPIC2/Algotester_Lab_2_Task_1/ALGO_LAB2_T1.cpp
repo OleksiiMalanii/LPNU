37483cb46d82c89b3b3480f1807e3f34ec3302d7
OleksiiMalanii <oleksii.malanii.shi.2023@lpnu.ua>
2023-10-31 00:40:18 +0200

    Comit All

---
+++ EPIC2/Algotester_Lab_2_Task_1/ALGO_LAB2_T1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int h[4], d[4];
+    for (int i = 0; i < 4; ++i) {
+        cin >> h[i];
+    }
+    for (int i = 0; i < 4; ++i) {
+        cin >> d[i];
+    }
+    int h_maxm h_min, h_max_i, h_min_i;
+}
